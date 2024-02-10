62c6a70dbc85aba7cae17b470ef007455580c668
{sho} <{keima9ke7i1ma5@gmail.com}>
2024-02-10 13:46:08 +0900

    コミット・プッシュの練習です

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -22,6 +22,11 @@ namespace StudentAccount.Controllers
         {
             return View();
         }
+        
+        public IActionResult ShouhinnAdd()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
---
+++ Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentAccount.Models
+{
+    public class Class
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string? Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string? Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
